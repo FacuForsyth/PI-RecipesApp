1c52cf18cdfa6d7d69be3e388f4696383d5258ab
FacuRamirez <framirezforsyth@gmail.com>
2022-09-16 12:34:13 -0300

    dotenv

--- client/src/index.js
+++ client/src/index.js
@@ -3,20 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import axios from 'axios';
 import { Provider } from 'react-redux';
 import { store } from './store';
-import dotenv from "dotenv"
-import axios from 'axios';
+import dotenv from "dotenv";
 dotenv.config();
 
 axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001";
 
 ReactDOM.render(
+  <React.StrictMode>
   <Provider store={store}>
     <React.StrictMode>
       <App />
-    </React.StrictMode>
-  </Provider>,
+    </React.StrictMode>,
+  </Provider>
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
