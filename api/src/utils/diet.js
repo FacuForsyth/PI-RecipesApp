072968ef395de6aa9661e4c744eb603560a2e27b
FacuForsyth <framirezforsyth@gmail.com>
2023-02-28 11:45:11 -0300

    change api key

--- api/src/utils/diet.js
+++ api/src/utils/diet.js
@@ -1,9 +1,9 @@
 const axios = require("axios");
 const { Recipe, Diet } = require("../db");
-const { API_KEY_6 } = process.env;
+const { API_KEY } = process.env;
 
 const getDietApi = async (req, res) => {
-    const dietsAPI = await axios.get(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY_6}&addRecipeInformation=true&number=50`);
+    const dietsAPI = await axios.get(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY}&addRecipeInformation=true&number=50`);
 
     const diets = dietsAPI.data?.results.map((rec) => rec.diets); 
     //console.log(diets)  [ [ 'gluten free', 'dairy free', 'lacto ovo vegetarian', 'vegan' ], [] ]
--- client/src/index.js
+++ client/src/index.js
@@ -9,8 +9,8 @@ import dotenv from "dotenv";
 dotenv.config();
 
 //axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001";
-//axios.defaults.baseURL = 'https://pi-recipesapp-production.up.railway.app/';
-axios.defaults.baseURL = "http://localhost:3001";
+axios.defaults.baseURL = 'https://pi-recipesapp-production.up.railway.app/';
+//axios.defaults.baseURL = "http://localhost:3001";
 
 ReactDOM.render(
   <React.StrictMode>
